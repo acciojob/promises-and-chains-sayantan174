7a7307152b059b3198a27179f49080dda8b8e2e4
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-20 11:22:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+document.getElementById("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  if(e.target.age.value>=18){
+    alert(`Welcome, ${e.target.name.value}. You can vote.`)
+  }
+  else{
+    alert(`Oh sorry ${e.target.name.value}. You aren't old enough.`)
+
+  }
+});
